91e837b72f4edcbce2a983517f2291ad4eeff302
Ishan Murphy <ishanpm@gmail.com>
2020-12-20 17:58:43 -0600

    lots of stuff - changed indentation - reorganized theorems - added operator properties section - added proofs - added pi calculus section

--- makeHtml.bat
+++ makeHtml.bat
@@ -1,5 +1,7 @@
 mkdir output
+del /q output\*
+xcopy html output /e/i
 cd output
-del /q *
-C:\metamath\metamath.exe "set scroll continuous" "read ..\linear.mm" "show statement * /html" "write theorem_list" "exit"
+C:\metamath\metamath.exe "set scroll continuous" "read ..\linear.mm" "show statement * /alt_html" "write theorem_list" "exit"
 cd ..
+pause
