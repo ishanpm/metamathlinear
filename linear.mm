03fd2593f4edf0544355cf22e2875ea6c873df61
Ishan Murphy <ishanpm@gmail.com>
2020-12-20 19:55:55 -0600

    commented out theorem that breaks mmj2

--- linear.mm
+++ linear.mm
@@ -632,7 +632,7 @@ $}
 ${
   lbsymi.1 $e |- ( ph O-O ps ) $.
   $( Linear biconditional is symmetric. Inference for ~lbsym . ERROR this makes mmj2 freak out for some reason $)
-  lbsymi $p |- ( ps O-O ph ) $= ? $.
+  $( lbsymi $p |- ( ps O-O ph ) $= ? $. $)
 $}
 ${
   lbtri.1 $e |- ( ph O-O ps ) $.
